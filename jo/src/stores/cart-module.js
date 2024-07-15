7eca16ce6996fae28fa01ba4075a1a22837a4169
Taiga ProZz <flo54@gmx.fr>
2024-07-15 16:34:19 +0200

    (feat) create module for store

---
+++ jo/src/stores/cart-module.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cartStore', {
+  state: () => ({
+    cart: {}
+  }),
+  getters: {
+    getCart(state) {
+      localStorage.getItem('cart') ? state.cart = JSON.parse(localStorage.getItem('cart')) : state.cart = [];
+      return state.cart;
+    },
+  },
+  actions: {
+    addItem(data) {
+      // check if cart is emmpty, if so, return
+      if (this.cart.length > 0) return;
+
+      // add item to localstorage and cart
+      localStorage.setItem('cart', JSON.stringify(data));
+      return this.cart = data;
+    },
+    removeItem() {
+      return this.cart = {};
+    }
+ 
+  },
+})

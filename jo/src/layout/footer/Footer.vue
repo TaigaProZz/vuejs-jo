d3a6aac13b74b7e4cb3542616dd31d4eb147f030
Taiga ProZz <flo54@gmx.fr>
2024-07-20 15:18:48 +0200

    (fix) Footer and ticket card : Fix routes link of profile of footer and fix height of ticket card

--- jo/src/layout/footer/Footer.vue
+++ jo/src/layout/footer/Footer.vue
@@ -43,7 +43,7 @@ export default  {
       secondSideElements: [
         { name: "A propos", href: "/about" },
         { name: "Formules", href: "/formules" },
-        { name: "Mon profil", href: "/account" },
+        { name: "Mon profil", href: "/profile" },
         { name: "© Copyright 2024. Made by Taiga ProZz", href: "https://github.com/taigaprozz/" },
       ]
     }

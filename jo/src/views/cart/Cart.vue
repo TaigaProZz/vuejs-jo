3aff83477a8dca0bbb82ff90afc817074db301a5
Taiga ProZz <flo54@gmx.fr>
2024-08-05 22:33:28 +0200

    (feat) fetch ticket infos in cart page

--- jo/src/views/cart/Cart.vue
+++ jo/src/views/cart/Cart.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col gap-10">
     <h1 class="text-4xl font-bold">Mon Panier</h1>
     
-    <CartCard v-if="Object.keys(getCart).length !== 0" :item="getCart" @delete-cart="handleRemoveItem" />
+    <CartCard v-if="Object.keys(getCart).length !== 0 && cart" :item="cart" @delete-cart="handleRemoveItem" />
     <h2 v-else class="text-xl">Votre panier est vide</h2>
   </div>
 </template>
@@ -11,6 +11,7 @@
 import { mapActions, mapState } from 'pinia';
 import { useCartStore } from '../../stores/cart-module';
 import CartCard from '../../components/cards/CartCard.vue';
+import { fetchOneTicket } from '../../services/ticket.service';
 
 export default {
   name: 'Cart',
@@ -28,9 +29,13 @@ export default {
   computed: {
     ...mapState(useCartStore, ['getCart']),
   },
+  async mounted() {    
+    if (Object.keys(this.getCart).length !== 0) {
+      this.cart = await fetchOneTicket(this.getCart.id);      
+    }
+  },
   methods: {
     ...mapActions(useCartStore, ['removeItem']),
-
     handleRemoveItem(item) {
       this.removeItem(item);
     }

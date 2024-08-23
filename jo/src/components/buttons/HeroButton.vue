8e200ab7dee55dcfce2b2c4d0cd5d41e0abbc089
Taiga ProZz <flo54@gmx.fr>
2024-08-23 16:55:02 +0200

    (feat) Buttons : set hover opacity

--- jo/src/components/buttons/HeroButton.vue
+++ jo/src/components/buttons/HeroButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="h-[48px] text-[16px] flex items-center justify-center text-text1 bg-creme rounded-md p-6 font-semibold">{{text}}</button>
+  <button class="h-[48px] text-[16px] flex items-center justify-center text-text1 bg-creme rounded-md p-6 font-semibold hover:opacity-75">{{text}}</button>
 </template>
 
 <script>
--- jo/src/components/buttons/PrimaryButton.vue
+++ jo/src/components/buttons/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="w-[120px] sm:w-[135px] h-[35px] sm:h-[45px] text-[16px] flex items-center justify-center bg-creme rounded-md font-semibold text-text1">{{text}}
+    class="w-[120px] sm:w-[135px] h-[35px] sm:h-[45px] text-[16px] flex items-center justify-center bg-creme rounded-md font-semibold text-text1 hover:opacity-75">{{text}}
   </button>
 
 </template>

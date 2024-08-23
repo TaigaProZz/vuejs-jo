8551d29b45d8925ab4fd6a8fb1ba2a9f270445c2
Taiga ProZz <flo54@gmx.fr>
2024-08-23 16:52:27 +0200

    (fix) Img home : set src/ for dev environment and none for prod

--- jo/src/views/home/Home.vue
+++ jo/src/views/home/Home.vue
@@ -79,19 +79,19 @@ export default {
           id: 1,
           title: "Athlétisme",
           hour: "10h00",
-          imgSrc: "src/assets/sports/athletics.jpg"
+          imgSrc: "/assets/sports/athletics.jpg"
         },
         {
           id: 2,
           title: "Natation",
           hour: "13h00",
-          imgSrc: "src/assets/sports/pool.jpg"
+          imgSrc: "/assets/sports/pool.jpg"
         },
         {
           id: 3,
           title: "Tir à l'arc",
           hour: "16h00",
-          imgSrc: "src/assets/sports/archery.jpg"
+          imgSrc: "/assets/sports/archery.jpg"
         },
       ],
       dayTwoSports: [
@@ -99,19 +99,19 @@ export default {
           id: 1,
           title: "Cyclysme",
           hour: "10h00",
-          imgSrc: "src/assets/sports/cyclisme.webp"
+          imgSrc: "/assets/sports/cyclisme.webp"
         },
         {
           id: 2,
           title: "Basket 3c3",
           hour: "13h00",
-          imgSrc: "src/assets/sports/basket.jpg"
+          imgSrc: "/assets/sports/basket.jpg"
         },
         {
           id: 3,
           title: "Golf",
           hour: "16h00",
-          imgSrc: "src/assets/sports/golf.jpg"
+          imgSrc: "/assets/sports/golf.jpg"
         },
       ]
     }
